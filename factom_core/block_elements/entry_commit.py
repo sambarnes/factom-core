223a774eeb9d2ace1f84168a2627c180e6f68819
Samuel J Vanderwaal <samuel.vanderwaal@gmail.com>
2019-10-21 15:43:48 -0800

    Add marshal_for_signature method to EntryCommit.

--- factom_core/block_elements/entry_commit.py
+++ factom_core/block_elements/entry_commit.py
@@ -11,16 +11,27 @@ class EntryCommit:
     entry_hash: bytes
     ec_spent: int
     ec_public_key: bytes
-    signature: bytes
+    _signature: bytes = None
 
     def __post_init__(self):
         # TODO: assert they're all here
         pass
 
+    @property
+    def signature(self):
+        return self._signature
+
+    @signature.setter
+    def signature(self, signature: bytes):
+        self._signature = signature
+
     def marshal(self):
         """Marshals the EntryCommit according to the byte-level representation shown at
         https://github.com/FactomProject/FactomDocs/blob/master/factomDataStructureDetails.md#entry-commit
         """
+        # Fail if signature is not set.
+        assert self._signature is not None
+
         buf = bytearray()
         buf.append(0x00)
         buf.extend(self.timestamp)
@@ -28,6 +39,14 @@ class EntryCommit:
         buf.append(self.ec_spent)
         buf.extend(self.ec_public_key)
         buf.extend(self.signature)
+        return bytes(buf)
+
+    def marshal_for_signature(self):
+        buf = bytearray()
+        buf.append(0x00)
+        buf.extend(self.timestamp)
+        buf.extend(self.entry_hash)
+        buf.append(self.ec_spent)
         return bytes(buf)
 
     @classmethod
